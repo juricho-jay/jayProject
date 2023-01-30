11f00cf5742ebc1334ff1a2d82ae067e7d67bbfe
juricho-jay <juri.cho.jay@gmail.com>
2023-01-30 19:05:17 +0900

    2023.01.30[jayCho]: map/set/iterator

--- cho/src/test/java/com/jay/ChoApplicationTests.java
+++ cho/src/test/java/com/jay/ChoApplicationTests.java
@@ -1,13 +1,52 @@
 package com.jay;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class ChoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void setTest() {
+		Map<String, String> map = new HashMap<>();
+
+		map.put("key1", "value1");
+		map.put("key3", "value3");
+		map.put("key2", "value2");
+
+		Set<String> set = map.keySet();
+
+		Iterator<String> ite = set.iterator();
+
+		List<String> keyList = new ArrayList<>();
+
+		while (ite.hasNext()) {
+			String key = ite.next();
+			keyList.add(key);
+		}
+
+		// key만 리스트로 뽑아내기
+		log.info("###keyList");
+		log.info(keyList.toString());
+
+		// map에 특정 키값이 있는지 조회하기
+		log.info("###containsKey");
+		System.out.println(map.containsKey("key1"));
+		log.info(String.valueOf(map.containsKey("key1")));
+
+		// map에 특정 value값이 있는지 조회하기
+		log.info("###containsValue");
+		log.info(String.valueOf(map.containsValue("value3243")));
 	}
 
 }
