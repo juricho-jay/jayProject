fbd553d45023608dca4ebf2c1fb6c3565cb7109a
juricho-jay <juri.cho.jay@gmail.com>
2023-02-19 22:27:27 +0900

    Feat[Front && Back]: Hello world Composition test

--- cho/src/main/frontend/src/App.js
+++ cho/src/main/frontend/src/App.js
@@ -2,9 +2,16 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
 
 function App() {
-  const [hello, setHello] = useState('')
+  const [hello, setHello] = useState('');
+  const name = "Jayri";
+  const naver = {
+    name: "네이버",
+    url: "https://naver.com",
+  };
 
   useEffect(() => {
       console.log("did it work?");
@@ -14,8 +21,24 @@ function App() {
   }, []);
 
   return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : 엥 {hello}
+      <div className="App">
+         {/* <h1
+            style={{
+                color: "#1741e0",
+                backgroundColor: "#00c4ff"
+            }}>
+              <p>
+                 백엔드에서 가져온 데이터입니다 : {hello}
+              </p>
+              <p>
+                 프론트의 정적 데이터입니다 : {name}
+              </p>
+
+              <a href={naver.url}>{naver.name}</a>
+          </h1>*/}
+
+          <Welcome />
+          <Hello />
       </div>
   );
 }
---
+++ cho/src/main/frontend/src/component/Hello.js
@@ -0,0 +1,7 @@
+import World from "./World";
+export default function Hello() {
+   return <div>
+               <h1>Hello!!! Nice to meet you!</h1>
+               <World />
+         </div>;
+}
